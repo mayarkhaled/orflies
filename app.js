5d8705f6e81417dc5c6b62f2412b673a26260b5d
mayarkhaled <miarkhaled3@gmail.com>
2021-01-03 12:49:58 -0800

    add backend folder

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+//mongo
 require('dotenv').config()
 var mongoose = require('mongoose');
 //Set up default mongoose connection
