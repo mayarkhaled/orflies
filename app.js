946900a781336afa08a02ffe331aab6c41e4d728
mayarkhaled <miarkhaled3@gmail.com>
2021-04-27 23:15:52 +0200

    routes updated

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/', collectionRouter);
+app.use('/collections', collectionRouter);
 app.use('/home',homeRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
